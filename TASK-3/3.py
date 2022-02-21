3ee7ee2b58b9dce89ea364a82d31c022258af586
Jyoshika11 <99651771+Jyoshika11@users.noreply.github.com>
2022-02-21 15:12:44 +0530

    Add files via upload

---
+++ TASK-3/3.py
@@ -0,0 +1,19 @@
+l1=["Roll","Name","Marks"]
+l2=["1","Jyos","90"]
+l3=["2","Shri","95"]
+l4=["3","Hari","85"]
+for i in l1:
+    print(i,end="   ")
+print()
+for i in l2:
+    print(i,end="     ")
+print()
+for i in l3:
+    print(i,end="     ")
+print()
+for i in l4:
+    print(i,end="     ")
+print()
+print("           ")
+for i in l3:
+    print(i,end="     ")
